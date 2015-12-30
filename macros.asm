bd12c2ec00d6f86465926ad88ae90ab2b511d42c
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2015-12-30 13:17:30 -0500

    Welcome to waterbear-snes, a framework for the easy development of SNES projects!

---
+++ macros.asm
@@ -0,0 +1,51 @@
+;u'heh
+;============================================================================
+;LoadPalette - Macro that loads palette information into CGRAM
+;Author: bazz, Neviksti, Marc
+;----------------------------------------------------------------------------
+; In: SRC_ADDR -- 24 bit address of source data,
+;     START -- Color # to start on,
+;     SIZE -- # of COLORS to copy
+;----------------------------------------------------------------------------
+; Out: None
+;----------------------------------------------------------------------------
+; Modifies: A,X
+; Requires: mem/A = 8 bit, X/Y = 16 bit
+;----------------------------------------------------------------------------
+.MACRO LoadPalette
+    lda #\2
+    sta $2121       ; Start at START color
+    lda #:\1        ; Using : before the parameter gets its bank.
+    ldx #\1         ; Not using : gets the offset address.
+    ldy #(\3 * 2)   ; 2 bytes for every color
+    jsr DMAPalette
+.ENDM
+
+;============================================================================
+; DMAPalette -- Load entire palette using DMA
+; Author: bazz
+;----------------------------------------------------------------------------
+; In: A:X  -- points to the data
+;      Y   -- Size of data
+;----------------------------------------------------------------------------
+; Out: None
+;----------------------------------------------------------------------------
+; Modifies: none
+;----------------------------------------------------------------------------
+DMAPalette:
+    phb
+    php         ; Preserve Registers
+
+    stx $4302   ; Store data offset into DMA source offset
+    sta $4304   ; Store data bank into DMA source bank
+    sty $4305   ; Store size of data block
+
+    stz $4300   ; Set DMA Mode (byte, normal increment)
+    lda #$22    ; Set destination register ($2122 - CGRAM Write)
+    sta $4301
+    lda #$01    ; Initiate DMA transfer
+    sta $420B
+
+    plp
+    plb
+    rts         ; return from subroutine
