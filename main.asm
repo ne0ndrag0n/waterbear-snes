0e2b460021a677f4eabee3aa3c43560e93c1066f
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-01-08 18:43:38 -0500

    ppu tests

--- main.asm
+++ main.asm
@@ -4,8 +4,6 @@
 .INCLUDE "ppu/ppu.asm"
 .INCLUDE "sys/system.asm"
 
-.EQU	PalNum		$0000
-
 .BANK 0 SLOT 0
 .ORG 0
 .SECTION "MainCode"
@@ -13,44 +11,30 @@
 Start:
         InitSNES            ; Init Snes :)
 
-		PPU_LoadPalette BG_Palette, 0, 14
-		PPU_LoadBlockToVRAMBytes Tiles, $0000, $0020
+		PPU_LoadPalette Demo16Palette, 0, 16
+		PPU_LoadBlockToVRAM Demo16Data, $0000, 3, 4
 
-		PPU_SetVRAMWriteParams TRUE, FALSE
+		PPU_SetVRAMWriteParams TRUE, PPU_IncRate_1x1
+
 		PPU_SetVRAMAddress $0400
 		PPU_WriteVRAM FALSE, $01, DIRECT
 
 		PPU_SetVRAMAddress $0401
 		PPU_WriteVRAM FALSE, $01, DIRECT
 
+		PPU_SetVRAMAddress $0402
+		PPU_WriteVRAM FALSE, $02, DIRECT
+
 		jsr SetupVideo
 
-		System_SetInterrupts TRUE, FALSE, FALSE, FALSE
+		;System_SetInterrupts TRUE, FALSE, FALSE, FALSE
 
 main:
-		System_Stall 6
-		lda PalNum
-		clc
-		adc #$04
-		and #$0C
-		sta PalNum
 
         jmp main
 
 ;============================================================================
 VBlank:
-
-	; Setup VRAM
-    PPU_SetVRAMWriteParams FALSE, FALSE
-
-    ; Set VRAM address
-    PPU_SetVRAMAddress $0400
-
-    ; Write to VRAM
-    PPU_WriteVRAM TRUE, PalNum, INDIRECT
-
-    stz $4210       ; Clear NMI flag
-
     RTI
 ;============================================================================
 
@@ -63,7 +47,7 @@ VBlank:
 ;----------------------------------------------------------------------------
 SetupVideo:
 
-    PPU_SetScreenMode PPU_Mode_0, FALSE, FALSE, FALSE, FALSE, FALSE
+    PPU_SetScreenMode PPU_Mode_1, FALSE, FALSE, FALSE, FALSE, FALSE
 
     PPU_SetTileMapAddr $01, PPU_TileMapSize_32x32, PPU_TILEMAP_ADDR_BG1
 
@@ -72,6 +56,10 @@ SetupVideo:
     PPU_SetSpriteAndTileLayers FALSE, TRUE, FALSE, FALSE, FALSE
 
     PPU_SetDisplay TRUE, $F
+
+    lda #$FF
+    sta $210E
+    sta $210E
 
     rts
 
