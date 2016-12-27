daf215a8e73d54336b1d31e75db02fd120d613e6
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-12-26 20:12:09 -0500

    vblank staging

--- main.asm
+++ main.asm
@@ -1,8 +1,9 @@
 ;== Include memorymap, header info, and SNES initialization routines
 .INCLUDE "header.inc"
 .INCLUDE "InitSNES.asm"
-.INCLUDE "ppu/ppu.asm"
-.INCLUDE "sys/system.asm"
+.INCLUDE "debug.inc"
+.INCLUDE "system.inc"
+.INCLUDE "ppu.inc"
 
 .BANK 0 SLOT 0
 .ORG 0
@@ -11,72 +12,69 @@
 Start:
         InitSNES            ; Init Snes :)
 
-		PPU_LoadPalette Demo16Palette, 0, 16
+				Stage_VBlank VBlank_Dynamic_Demo
 
-		PPU_LoadBlockToVRAM FontData4BPP, PPU_BG1CharacterAddr, 96, 4
-		PPU_LoadBlockToVRAM Demo16Data, PPU_BG2CharacterAddr, 2, 4
+				lda #%10000000
+				sta $4200				; enable the vblank
 
-		jsr SetupVideo
-
-		; Putting this after SetupVideo to test...
-		PPU_FillTileMap PPU_BG1TileMapAddr, $00, 96, TRUE, TRUE
-		PPU_FillTileMap PPU_BG1TileMapAddr + 96, $00, 96, TRUE, TRUE
-		PPU_FillTileMap PPU_BG1TileMapAddr + ( 96 * 2 ), $00, 96, TRUE, TRUE
-		PPU_FillTileMap PPU_BG1TileMapAddr + ( 96 * 3 ), $00, 96, TRUE, TRUE
-		PPU_FillTileMap PPU_BG1TileMapAddr + ( 96 * 4 ), $00, 96, TRUE, TRUE
-		PPU_FillTileMap PPU_BG1TileMapAddr + ( 96 * 5 ), $00, 96, TRUE, TRUE
-		PPU_FillTileMap PPU_BG1TileMapAddr + ( 96 * 6 ), $00, 96, TRUE, TRUE
-		PPU_FillTileMap PPU_BG1TileMapAddr + ( 96 * 7 ), $00, 96, TRUE, TRUE
-		PPU_FillTileMap PPU_BG1TileMapAddr + ( 96 * 8 ), $00, 96, TRUE, TRUE
-		PPU_FillTileMap PPU_BG1TileMapAddr + ( 96 * 9 ), $00, 32, TRUE, TRUE
 main:
-
         jmp main
 
-;============================================================================
+;==============================================================================
+; MAX 2270 cycles.
+; Push and pop anything you're going to fuck up.
 VBlank:
-	phb
+	pha
 	php
+	phb
 
-	System_SetAccumulatorSize 	System_REGISTER_BYTE
-	System_SetIndexSize       	System_REGISTER_WORD
+	Set_A_16Bit
+	lda.w VBlank_Function_Pointer
+	cmp #$0000
+	beq VBlank_Exit
 
-    plp
-    plb
-    RTI
-;============================================================================
+Handler:
+	; Branch to this function pointer as a subroutine, then clear out the pointer.
+	; It's up to you to "jmp VBlank_Finally" when you're all done.
+	jmp (VBlank_Function_Pointer)
 
-;============================================================================
-; SetupVideo -- Sets up the video mode and tile-related registers
-;----------------------------------------------------------------------------
-; In: None
-;----------------------------------------------------------------------------
-; Out: None
-;----------------------------------------------------------------------------
-SetupVideo:
+VBlank_Finally:
+	; After a function is called, set it to null so it doesn't get called again on next VBlank.
+	Set_A_16Bit
+	lda.w #$0000
+	sta VBlank_Function_Pointer ; Rip this shit down on the way out
 
-    PPU_SetScreenMode PPU_PrimaryScreenMode, FALSE, FALSE, FALSE, FALSE, FALSE
+VBlank_Exit:
+	plb
+	plp
+	pla
+  RTI
+;==============================================================================
 
-    PPU_SetTileMapAddr PPU_BG1TileMapIndex, PPU_PrimaryTileMapSize, PPU_TILEMAP_ADDR_BG1
-
-    PPU_SetCharAddr PPU_BG1BG2, PPU_BG1CharacterSet, PPU_BG2CharacterSet
-
-    PPU_SetSpriteAndTileLayers FALSE, TRUE, FALSE, FALSE, FALSE
-
-	; write waterbear PPU_Scroll* functions
-    lda #$FF
-    sta $210E
-    sta $210E
-
-    rts
+;==============================================================================
+; load $DEAD to $0010 as a demo of vblank dynamic dispatch
+VBlank_Dynamic_Demo:
+	lda #$0BB0
+	sta $0010
+	jmp VBlank_Finally
 
 .ENDS
 
+; stuff we'll revisit later
 .BANK 1
 .ORG 0
-.SECTION "TileData"
+.SECTION "Data"
 
-	.INCLUDE "res/tilesets/testfaces.inc"
-	.INCLUDE "res/tilesets/charset.inc"
+; 2bpp
+
+TileData:
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $00, $FF, $00, $FF, $FF, $00, $FF, $FF, $FF, $FF, $FF, $00, $00, $FF, $00, $FF
+
+Palette:
+    .db $00, $00, $1F, $42, $E0, $7F, $FF, $7F
+
+CArcadiaCopr:	.DB "Copyright (c) Corvis Simulation Systems. All rights reserved.",0
+CArcadia: .DB "Corvis Arcadia",0
 
 .ENDS
